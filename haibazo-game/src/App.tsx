e247f190349366b5eb813e758e664125993b4c4a
vantruyenn <nguyenvantruyen11.42021@gmail.com>
2026-04-23 00:56:59 +0700

    first commit

---
+++ haibazo-game/src/App.tsx
@@ -0,0 +1,160 @@
+import { useEffect, useRef, useState } from 'react';
+import './App.css'
+import Circle from './circle';
+
+function App() {
+  
+  //UI state
+  const [input, setInput] = useState<number>(5)
+  const [positions, setPosition] = useState<{order: number, x:number, y: number}[]>([])
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [autoPlay, setAutoPlay] = useState(false)
+  const [stopCircleTimer, setStopCircleTimer] = useState(false)
+
+  //Data state
+  const [totalTime, setTotalTime] = useState(0)
+  const [playingStatus, setPlayingStatus] = useState<'play'| 'game-over'|'all-cleared'>('play')
+  const [totalSelected, setTotalSelected] = useState(0)
+  const [nextOrder, setNextOrder] = useState(1);
+
+  const timerRef = useRef<number|null>(null)
+
+  const stopTimer = () => {
+    clearInterval(timerRef.current);
+  };
+
+  useEffect(()=>{
+    if(totalSelected === input)
+      setTimeout(()=>{
+        stopTimer();
+        setPlayingStatus('all-cleared')
+      }, 3000)
+  }, [totalSelected, input])
+
+  const mainWidth = 500, mainHeight = 500;
+  const diameter = 40;
+
+
+  const ranDomPosition = ()=>{
+    const x = Math.random() * (mainWidth - diameter)
+    const y = Math.random() * (mainHeight - diameter)
+    return {x, y};
+  }
+
+  const renderCircles = ()=>{
+    for(let i = 1; i <= input; i++){
+      const posistion = ranDomPosition()
+      setPosition(pre => [...pre, {order: i, ...posistion}]);
+    }
+  }
+
+  const handlPlaying = ()=>{
+
+    if(!isPlaying){
+      setIsPlaying(true)
+      console.log('Start game')
+
+      renderCircles();
+
+      timerRef.current = setInterval(()=>{
+        setTotalTime(pre =>pre+=100)
+      }, 100)
+    }
+    else if(isPlaying && playingStatus === 'play'){
+
+      console.log('Restart during playing')
+
+      stopTimer()
+
+      setPosition([]);
+      setTotalSelected(0);
+      setPlayingStatus('play')
+      setTotalTime(0);
+      setNextOrder(1);
+      setAutoPlay(false)
+      setStopCircleTimer(false)
+
+      renderCircles();
+
+      timerRef.current = setInterval(()=>{
+        setTotalTime(pre =>pre+=100)
+      }, 100)
+
+    }
+    else {
+      setIsPlaying(false)
+      console.log('Restart after done all cleared')
+
+      stopTimer()
+      setPosition([])
+      setTotalTime(0)
+      setTotalSelected(0);
+      setPlayingStatus('play')
+      setTotalTime(0);
+      setNextOrder(1);
+      setAutoPlay(false)
+      setStopCircleTimer(false)
+    }
+
+  }
+
+  const handlAutoPlay = ()=>{
+    setAutoPlay(pre=>!pre)
+  }
+
+  return (
+    <>
+      <header className="header">
+        <h3 style={{color:playingStatus==='play'?'darkgray':playingStatus==='all-cleared'?'darkgreen':'darkred'}}>
+          {playingStatus==='play'?'Let\'t play':playingStatus==='all-cleared'?'All Cleared' : 'Game Over'}
+        </h3>
+        <div className='wapper-input'>
+          <div className="lable">Points:</div>
+          <input value={input} type="number" onChange={(e)=>setInput(e.target.value as unknown as number)}/>
+        </div>
+        <div className='wapper-input'>
+          <div className="lable">Time:</div>
+          <span className='time'>{(totalTime/1000).toFixed(1)}s</span>
+        </div>
+        <div style={{display:'flex', gap:'10px'}}>
+          <button onClick={handlPlaying}>{isPlaying ? 'Restart' : 'Play'}</button>
+          {(isPlaying && playingStatus==='play') && <button onClick={handlAutoPlay}>Auto Play {autoPlay ? 'OFF' : 'ON'}</button>}
+        </div>
+      </header>
+
+      <main className="main"
+        style={{width:mainWidth, height:mainHeight}}
+      >
+        {
+          positions.map(item => {
+            return <Circle
+              key={item.order}
+              diameter={diameter}
+              order={item.order}
+              totalPoints={input}
+              topPosition={item.y}
+              leftPosition={item.x}
+              isStopTimer={stopCircleTimer}
+              autoPlay={autoPlay && item.order===nextOrder}
+              onSelect={(order:number)=>{
+                if(order===nextOrder){
+                  setNextOrder(pre=>pre+=1);
+                }
+                else{
+                  setPlayingStatus('game-over');
+                  stopTimer()
+                  setStopCircleTimer(true)
+                }
+              }}
+              onCircleGone={(order:number)=>setTotalSelected(pre=>pre+=order)}
+            />
+          }
+        )
+        }
+      </main>
+
+    </>
+  )
+}
+
+export default App
---
+++ haibazo-game/src/circle.tsx
@@ -0,0 +1,67 @@
+import { useState, useRef, useEffect } from 'react';
+import './circle.css' ;
+
+interface Cicle{
+  diameter:number,
+  order: number,
+  totalPoints:number,
+  topPosition: number,
+  leftPosition: number,
+  isStopTimer:boolean,
+  autoPlay:boolean,
+  onSelect: (order:number)=>void,
+  onCircleGone: (order:number)=>void,
+}
+
+export default function Circle({diameter, order, totalPoints , topPosition, leftPosition, isStopTimer, autoPlay, onSelect, onCircleGone}:Cicle) {
+
+  const [remainingTime, setRemainingTime] = useState<number>(3000)
+  const [selected, setSelected] = useState(false)
+
+  const timerRef = useRef<number|null>(null)
+
+  function stopTimer(){
+    clearInterval(timerRef.current)
+  }
+
+  useEffect(()=>{
+    if(remainingTime-100===0)
+      stopTimer()
+
+    if(isStopTimer)
+      stopTimer()  
+  }, [remainingTime])
+
+  useEffect(()=>{
+    if(autoPlay){
+      const timeout = setTimeout(()=>{
+        handlSelect()
+      }, 1000)
+      return () => clearTimeout(timeout)
+    }
+  }, [autoPlay])
+
+  function handlSelect(){
+    if(selected || isStopTimer) return;
+    setSelected(true);
+
+    onSelect(order)
+    onCircleGone(1)
+
+    timerRef.current = setInterval(()=>{
+      setRemainingTime(pre=>pre-100)
+    }, 100)
+  }
+
+
+  return (
+    <button 
+      className={`cicle ${remainingTime <= 1000 ? 'fade-out' : ''} ${selected ? 'selected' : ''}`}
+      style={{width:diameter, height:diameter, top:topPosition, left:leftPosition, zIndex:selected ? order + totalPoints : order}}
+      onClick={handlSelect}
+    >
+        <div className="circle-order">{order}</div>  
+        {(remainingTime > 0 && selected) && <div className={`remaining-time ${selected && 'remaintim-time-on_selected'}`}>{(remainingTime/1000).toFixed(1)}s</div>}
+    </button>
+  )
+}
